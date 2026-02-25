6ff2fc5acdf4c1ab0c5be9c322b8ad0e421539cb
kangkans59-collab <kangkans59@gmail.com>
2026-02-25 16:16:08 +0530

    footer

--- Hack/src/App.jsx
+++ Hack/src/App.jsx
@@ -2,11 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
+import { Box, CssBaseline } from '@mui/material'; // Box must be here!
+import Navbar from './Components/Navbar/Navbar.jsx'
+import Footer from './Components/Navbar/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  
+  return(
+   <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Navbar />
+      
+      {/* Main Content Area */}
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {/*<h1>Welcome to the Dashboard</h1>*/}
+        {/* Your other page components go here */}
+      </Box>
+
+      <Footer />
+    </Box>
+  )
   
 }
 
