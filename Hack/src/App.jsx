725cd4e0359bf872b61b6019b1f99479303e39ec
BarnamB <boruahbarnam@gmail.com>
2026-02-25 15:55:44 +0530

    Made the home page

--- Hack/src/App.jsx
+++ Hack/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import UploadButton from './Components/UploadButton/UploadButton';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  
+  return (
+    <div className="App">
+      <UploadButton />
+    </div>
+  );
   
 }
 
